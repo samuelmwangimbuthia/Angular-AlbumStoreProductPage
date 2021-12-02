ea280e9cbe88b2e6bc6fb872d7183f1b4d4dac74
Samuel Mwangi Mbuthia <mbuthia.jsamuel@gmail.com>
2021-12-02 07:20:31 +0300

    part 3

---
+++ src/app/product-service.ts
@@ -0,0 +1,17 @@
+import { HttpClient,HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import 'rxjs/operators';
+
+
+@Injectable()
+export class ProductService {
+private _albumUrl = '../assets/album.json';
+
+constructor(private _http: HttpClient) { }
+
+  getAlbum(id:number){
+    return this._http.get(this._albumUrl).
+    map(response => response)
+  }
+}
