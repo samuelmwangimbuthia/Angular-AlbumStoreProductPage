45623a18470dc0556764cb7989d587025988bdea
Samuel Mwangi Mbuthia <mbuthia.jsamuel@gmail.com>
2021-12-16 06:01:28 +0300

    completed part 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-import { ProductService } from './product-service';
+import { ProductService } from './product.service';
 
 @NgModule({
   declarations: [
@@ -16,8 +16,9 @@ import { ProductService } from './product-service';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
