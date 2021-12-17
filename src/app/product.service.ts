59d786f8d6c25189594d129b1dd01e3145a857cb
Samuel Mwangi Mbuthia <mbuthia.jsamuel@gmail.com>
2021-12-17 06:14:40 +0300

    implimenting strong typing interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,6 +12,6 @@ constructor(private _http: Http) { }
 
   getAlbum(id:number):Observable<Album>{
     return  this._http.get(this._albumUrl)
-    .map((response) => response.json());
+    .map((response) => <Album>response.json());
   }
 }
